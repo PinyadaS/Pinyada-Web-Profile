04a2e63d10788382350b2bf2f559c2ae41301580
PinyadaS <155807141+PinyadaS@users.noreply.github.com>
2024-05-11 16:13:59 +0700

    init profile page
    create profile page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import ResponsiveAppBar from './components/ResponsiveAppBar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Profile from './pages/Profile';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <ResponsiveAppBar/>
-    </div>
+    <ResponsiveAppBar />
+      <Routes>
+        <Route path='/'element={<Profile/>}/>
+      </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,14 @@
+import { Container, Typography } from "@mui/material";
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div>
+            <Container maxWidth='lg'>
+            <Typography textAlign='center' marginTop='30px'>Welcome!</Typography>
+            </Container>
+        </div>
+    )
+}
+
+export default Profile
